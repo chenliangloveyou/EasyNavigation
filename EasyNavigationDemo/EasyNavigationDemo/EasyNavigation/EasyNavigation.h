0319e7d5d454c170e1011bef214aba8728a3d303
chenliangloveyou <ios_elite@163.com>
2017-09-03 22:49:06 +0800

    commit

---
+++ EasyNavigationDemo/EasyNavigationDemo/EasyNavigation/EasyNavigation.h
@@ -0,0 +1,13 @@
+//
+//  EasyNavigation.h
+//  EasyNavigationDemo
+//
+//  Created by Mr_Chen on 17/9/3.
+//  Copyright © 2017年 chenliangloveyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EasyNavigation : UIView
+
+@end
---
+++ EasyNavigationDemo/EasyNavigationDemo/EasyNavigation/EasyNavigation.m
@@ -0,0 +1,21 @@
+//
+//  EasyNavigation.m
+//  EasyNavigationDemo
+//
+//  Created by Mr_Chen on 17/9/3.
+//  Copyright © 2017年 chenliangloveyou. All rights reserved.
+//
+
+#import "EasyNavigation.h"
+
+@implementation EasyNavigation
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
